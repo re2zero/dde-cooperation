4645dd0473a49008163d209a7fffbdd24f65584e
re2zero <yangwu@uniontech.com>
2024-04-28 16:25:35 +0800

    fix: [build]fix asio for msvc
    It needs build out as static library for all base module, this miss asio.
    
    Log: fix msvc build issue.

--- 3rdparty/CppServer/modules/asio.cmake
+++ 3rdparty/CppServer/modules/asio.cmake
@@ -2,11 +2,11 @@ if(NOT TARGET asio)
 
   # Module library
   file(GLOB SOURCE_FILES "asio/asio/src/*.cpp")
-  add_library(asio ${SOURCE_FILES})
+  add_library(asio STATIC ${SOURCE_FILES})
   if(MSVC)
     set_target_properties(asio PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS}")
   else()
-    set_target_properties(asio PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} -Wno-shadow -fPIC")
+    set_target_properties(asio PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} -Wno-shadow")
   endif()
   target_compile_definitions(asio PRIVATE ASIO_STANDALONE ASIO_SEPARATE_COMPILATION)
   target_include_directories(asio PUBLIC "asio/asio/include" PUBLIC ${OPENSSL_INCLUDE_DIR})
