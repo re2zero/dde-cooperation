368fd1d5ef06f2744c6ff1bc6a433c9b256b8d8d
re2zero <yangwu@uniontech.com>
2024-05-17 16:58:36 +0800

    refactor: [build]debian build
    refactor the debian build and fix some issue.
    
    Log: enable debian build.

--- modules/QtZeroConf.cmake
+++ modules/QtZeroConf.cmake
@@ -3,6 +3,9 @@ if(NOT TARGET QtZeroConf)
   # Module subdirectory
   set(QTZEROCONF_DIR "${PROJECT_SOURCE_DIR}/3rdparty/QtZeroConf")
 
+  # set build as share library
+  set(BUILD_SHARED_LIBS ON)
+
   # Module subdirectory
   add_subdirectory("${QTZEROCONF_DIR}" QtZeroConf)
 endif()
